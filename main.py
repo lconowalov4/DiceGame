7b2f1b134f2492732f0da94d3506587e7576c781
lconowalov4 <rw6lfe@gmail.com>
2025-07-07 23:25:24 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import random
+import time as t
+
+def action():
+    h_sum = b_sum = 0
+    print("Start playing the game now!")
+    t.sleep(1)
+    for dice in range(0, choice):
+        person = random.randint(1,6)
+        t.sleep(1)
+        print("Human got: ", person)
+        h_sum += person
+t.sleep(1)
+print("\n Total sum of Human is: ", h_sum)
+t.sleep(1)
+print("\n Now it's the turn for the bot")
+t.sleep(1)
+
