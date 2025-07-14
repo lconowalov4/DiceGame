4d6ab809553a5a02f8294f20c8ab2599e43aa1c2
lconowalov4 <rw6lfe@gmail.com>
2025-07-15 00:06:10 +0300

    Added action for bot

--- main.py
+++ main.py
@@ -16,3 +16,11 @@ t.sleep(1)
 print("\n Now it's the turn for the bot")
 t.sleep(1)
 
+for dice in range(0, choice):
+    bot = random.randint(1,6)
+    t.sleep(1)
+    print("Bot got: ", bot)
+    b_sum += bot
+t.sleep(1)
+print("\n Total sum of Bot is: ", b_sum)
+t.sleep(2)
